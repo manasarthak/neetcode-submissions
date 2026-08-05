394cda986a46e6cc9d7592d062821d9c69f49051
Sarthak Singh <61450665+manasarthak@users.noreply.github.com>
2026-08-05 12:49:06 -0700

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        queue=deque()
+        rows,cols=len(grid),len(grid[0])
+        directions=[(-1,0),(1,0),(0,-1),(0,1)]
+        visited=[[False for _ in range(cols)] for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]==0:
+                    queue.append([i,j])
+                    visited[i][j]=True
+                    
+        depth=0
+        inf=2147483647
+        while queue:
+            ln=len(queue)
+            for i in range(ln):
+                m,n=queue.popleft()
+                if grid[m][n]==inf:
+                    grid[m][n]=depth
+                for direction in directions:
+                    x,y=m+direction[0],n+direction[1]
+                    if x>=0 and y>=0 and x<rows and y<cols and grid[x][y]!=-1 and not visited[x][y]:
+                        queue.append([x,y])
+                        visited[x][y]=True
+            depth+=1
+        
+
+
+
+
+        
