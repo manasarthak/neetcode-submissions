04b6bf21e394e64902d2fcba68b65a7b44d7abc0
Sarthak Singh <61450665+manasarthak@users.noreply.github.com>
2026-09-08 16:04:46 -0700

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        #first we write the recursive solution(decision tree)
+        n=len(s)
+        dp=[0]*(n+1)
+        dp[n]=1
+        for i in range(n-1,-1,-1):
+            if s[i]=='0':
+                continue
+            dp[i]=dp[i+1]
+            if i+1<n and int(s[i:i+2])<=26:
+                dp[i]+=dp[i+2]
+        return dp[0]
